91ed9c29f773d0207e6ffba55da8c9bc013ce078
Alexander Schiftner <alex@shapediver.com>
2024-06-23 09:33:54 +0200

    SS-7777 fix for non-unique value list options

--- components/shapediver/parameter/ParameterSelectComponent.tsx
+++ components/shapediver/parameter/ParameterSelectComponent.tsx
@@ -1,5 +1,5 @@
 import { MultiSelect, Select } from "@mantine/core";
-import React from "react";
+import React, { useMemo } from "react";
 import ParameterLabelComponent from "./ParameterLabelComponent";
 import { PropsParameter } from "../../../types/components/shapediver/propsParameter";
 import { useParameterComponentCommons } from "../../../hooks/shapediver/parameters/useParameterComponentCommons";
@@ -20,31 +20,47 @@ export default function ParameterSelectComponent(props: PropsParameter) {
 		disabled
 	} = useParameterComponentCommons<string>(props, 0);
 
+	// We need to prevent duplicate values in definition choices
+	// and append a numeric postfix to duplicate items to make them unique
+	const uniqueChoices = useMemo(() => {
+		if (!definition.choices) return [];
+		const uniqueChoices: string[] = [];
+		const choiceCounts: { [key: string]: number } = {};
+		definition.choices.forEach((choice) => {
+			if (choiceCounts[choice] === undefined) {
+				choiceCounts[choice] = 0;
+			} else {
+				choiceCounts[choice]++;
+			}
+			uniqueChoices.push(choiceCounts[choice] > 0 ? `${choice} ${choiceCounts[choice]}` : choice);
+		});
+		
+		return uniqueChoices;
+	}, [definition.choices]);
+	
 	const inputComponent = definition.visualization === ShapeDiverResponseParameterVisualization.CHECKLIST
 		? <MultiSelect
-			value={value ? value.split(",").map((v) => definition.choices![parseInt(v)]) : []}
+			value={value ? value.split(",").map((v) => uniqueChoices[parseInt(v)]) : []}
 			onChange={(v) => {
-				handleChange(definition.choices ?
-					definition.choices
-						// Collect indexes and values
-						.map((value, index) => ({ index, value }))
-						// Filter by values
-						.filter((obj) => v.includes(obj.value))
-						// Return filtered indexes
-						.map((obj) => obj.index)
-						.join(",")
-					: ""
+				handleChange(uniqueChoices
+					// Collect indexes and values
+					.map((value, index) => ({ index, value }))
+					// Filter by values
+					.filter((obj) => v.includes(obj.value))
+					// Return filtered indexes
+					.map((obj) => obj.index)
+					.join(",")
 				);
 			}}
-			data={definition.choices!}
+			data={uniqueChoices}
 			disabled={disabled}
 			comboboxProps={{withinPortal: false}}
 		/>
 		: <Select
 			allowDeselect={false}
-			value={definition.choices![+value]}
-			onChange={(v) => handleChange(definition.choices!.indexOf(v!) + "")}
-			data={definition.choices!}
+			value={uniqueChoices[+value]}
+			onChange={(v) => handleChange(uniqueChoices.indexOf(v!) + "")}
+			data={uniqueChoices}
 			disabled={disabled}
 			comboboxProps={{withinPortal: false}}
 		/>;
