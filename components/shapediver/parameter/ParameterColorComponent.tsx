3e5490c404a5a2e6e519e8663f8a0154252960be
Alexander Schiftner <alex@shapediver.com>
2024-06-24 16:11:01 +0200

    improve manual color entry

--- components/shapediver/parameter/ParameterColorComponent.tsx
+++ components/shapediver/parameter/ParameterColorComponent.tsx
@@ -1,12 +1,16 @@
 import { ActionIcon, ColorInput } from "@mantine/core";
 import { IconRefresh } from "@tabler/icons-react";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import ParameterLabelComponent from "./ParameterLabelComponent";
 import { PropsParameter } from "../../../types/components/shapediver/propsParameter";
 import { useParameterComponentCommons } from "../../../hooks/shapediver/parameters/useParameterComponentCommons";
 
 function convertFromSdColor(val: string) {
 	return val.replace("0x", "#").substring(0, 7);
+}
+
+function convertToSdColor(val: string) {
+	return val.replace("#", "0x") + "ff";
 }
 
 /**
@@ -19,25 +23,31 @@ export default function ParameterColorComponent(props: PropsParameter) {
 	const {
 		definition,
 		handleChange,
-		value,
+		value: paramValue,
 		onCancel,
 		disabled
 	} = useParameterComponentCommons<string>(props, 0, state => state.uiValue);
 
 	const handleSdColorChange = useCallback((val: string) => {
-		handleChange(val.replace("#", "0x") + "ff");
+		handleChange(convertToSdColor(val));
 	}, [handleChange]);
+
+	const [value, setValue] = useState(convertFromSdColor(paramValue));
+	useEffect(() => {
+		setValue(convertFromSdColor(paramValue));
+	}, [paramValue]);
 
 	return <>
 		<ParameterLabelComponent { ...props } cancel={onCancel} />
 		{ definition && <ColorInput
 			placeholder="Pick color"
-			value={convertFromSdColor(value)}
+			value={value}
 			rightSection={
 				<ActionIcon onClick={() => handleChange(definition.defval)}>
 					<IconRefresh size={16} />
 				</ActionIcon>
 			}
+			onChange={setValue}
 			onChangeEnd={handleSdColorChange}
 			disabled={disabled}
 			popoverProps={{withinPortal: false}}
