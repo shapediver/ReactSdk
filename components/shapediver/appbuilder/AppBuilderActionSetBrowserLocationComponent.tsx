5fb6e62c40403b3341e9d54c33b7e9f1b0aaec89
Alexander Schiftner <alex@shapediver.com>
2024-09-05 13:29:51 +0200

    implement "setBrowserLocation" action

--- components/shapediver/appbuilder/AppBuilderActionSetBrowserLocationComponent.tsx
+++ components/shapediver/appbuilder/AppBuilderActionSetBrowserLocationComponent.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { IAppBuilderActionPropsSetBrowserLocation } from "../../../types/shapediver/appbuilder";
 import AppBuilderActionComponent from "./AppBuilderActionComponent";
 
 type Props = IAppBuilderActionPropsSetBrowserLocation;
+
+function getLocation(href?: string, pathname?: string, search?: string, hash?: string) : string {
+	if (href)
+		return href;
+	
+	const currentLocation = window.location;
+
+	if (pathname)
+		return `${currentLocation.origin}${pathname.startsWith("/") ? pathname : "/" + pathname}`;
+	
+	if (search) 
+		return `${currentLocation.origin}${currentLocation.pathname}${search.startsWith("?") ? search : "?" + search}`;
+	
+	if (hash) 
+		return `${currentLocation.origin}${currentLocation.pathname}${currentLocation.search}${hash.startsWith("#") ? hash : "#" + hash}`;
+	
+	return currentLocation.href;
+}
 
 /**
  * Functional component for a "setBrowserLocation" action.
@@ -11,13 +29,21 @@ type Props = IAppBuilderActionPropsSetBrowserLocation;
  */
 export default function AppBuilderActionSetBrowserLocationComponent(props: Props) {
 	
-	const { label = "Set location", icon, tooltip } = props;
-	
-	// TODO: Implement the action
+	const { label = "Set location", icon, tooltip, href, pathname, search, hash, target } = props;
+
+	const onClick = useCallback(() => {
+		const newLocation = getLocation(href, pathname, search, hash);
+		if (target && target !== "_self") {
+			window.open(newLocation, target);
+		} else if (newLocation !== window.location.href) {
+			window.location.href = newLocation;
+		}
+	}, [href, pathname, search, hash, target]);
 
 	return <AppBuilderActionComponent 
 		label={label}
 		icon={icon}
 		tooltip={tooltip}
+		onClick={onClick}
 	/>;
 }
