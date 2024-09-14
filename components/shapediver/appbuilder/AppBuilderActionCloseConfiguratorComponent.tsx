6e58aa98c8db91e8b6667add904d7ced9edb327f
Alexander Schiftner <alex@shapediver.com>
2024-09-14 19:28:02 +0200

    closeConfigurator action

---
+++ components/shapediver/appbuilder/AppBuilderActionCloseConfiguratorComponent.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback, useContext } from "react";
+import { IAppBuilderActionPropsCloseConfigurator } from "../../../types/shapediver/appbuilder";
+import AppBuilderActionComponent from "./AppBuilderActionComponent";
+import { ECommerceApiSingleton } from "../../../modules/ecommerce/singleton";
+import { NotificationContext } from "../../../context/NotificationContext";
+
+type Props = IAppBuilderActionPropsCloseConfigurator & {
+};
+
+/**
+ * Functional component for an "addToCart" action.
+ *
+ * @returns
+ */
+export default function AppBuilderActionCloseConfiguratorComponent(props: Props) {
+	
+	const { 
+		label = "Close configurator", 
+		icon, 
+		tooltip, 
+	} = props;
+	const notifications = useContext(NotificationContext);
+
+	const onClick = useCallback(async () => {
+		// in case we are not running inside an iframe, the instance of 
+		// IEcommerceApi will be a dummy for testing
+		const api = await ECommerceApiSingleton;
+		const result = await api.closeConfigurator();
+		if (!result)
+			notifications.show({message: "Could not close configurator."});
+	}, []);
+
+	return <AppBuilderActionComponent 
+		label={label}
+		icon={icon}
+		tooltip={tooltip}
+		onClick={onClick}
+	/>;
+}
