f9c1d764e0a691c45d55de33b1bb1385be9c3d9d
Alexander Schiftner <alex@shapediver.com>
2024-09-20 16:56:17 +0200

    add screenshot to model state

--- components/shapediver/appbuilder/AppBuilderActionAddToCartComponent.tsx
+++ components/shapediver/appbuilder/AppBuilderActionAddToCartComponent.tsx
@@ -4,6 +4,8 @@ import AppBuilderActionComponent from "./AppBuilderActionComponent";
 import { ECommerceApiSingleton } from "../../../modules/ecommerce/singleton";
 import { NotificationContext } from "../../../context/NotificationContext";
 import { useShapeDiverStoreViewer } from "../../../store/useShapeDiverStoreViewer";
+import { useViewportId } from "../../../hooks/shapediver/viewer/useViewportId";
+import { useShallow } from "zustand/react/shallow";
 
 type Props = IAppBuilderActionPropsAddToCart & {
 	sessionId: string;
@@ -25,15 +27,27 @@ export default function AppBuilderActionAddToCartComponent(props: Props) {
 		quantity,
 		price,
 		description,
+		//image, // TODO use image defined by export of href
+		createGltf
 	} = props;
-	const sessionApi = useShapeDiverStoreViewer(state => state.sessions[sessionId]);
+	const { viewportId } = useViewportId();
+	const { sessionApi, viewportApi } = useShapeDiverStoreViewer(useShallow(state => ({
+		sessionApi: state.sessions[sessionId],
+		viewportApi: state.viewports[viewportId],
+	})));
 	const notifications = useContext(NotificationContext);
 
 	const onClick = useCallback(async () => {
 		// in case we are not running inside an iframe, the instance of 
 		// IEcommerceApi will be a dummy for testing
 		const api = await ECommerceApiSingleton;
-		const modelStateId = await sessionApi.createModelState();
+		const modelStateId = await sessionApi.createModelState(
+			undefined, // <-- use parameter values of the session
+			false, // <-- use parameter values of the session
+			() => viewportApi.getScreenshot(),
+			undefined, // <-- custom data
+			createGltf ? async () => viewportApi.convertToGlTF() : undefined
+		);
 		try {
 			const result = await api.addItemToCart({
 				modelStateId,
@@ -54,6 +68,9 @@ export default function AppBuilderActionAddToCartComponent(props: Props) {
 		quantity,
 		price,
 		description,
+		sessionApi,
+		viewportApi,
+		createGltf
 	]);
 
 	return <AppBuilderActionComponent 
