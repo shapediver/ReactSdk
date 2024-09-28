1336d4778e60e811a3729518e7b3116ffe2347b9
Alexander Schiftner <alex@shapediver.com>
2024-09-28 12:16:32 +0200

    analytics connector

--- context/TrackerContext.ts
+++ context/TrackerContext.ts
@@ -12,3 +12,23 @@ export const DummyTracker: ITrackerContext = {
 
 /** Information about a template. */
 export const TrackerContext = createContext<ITrackerContext>(DummyTracker);
+
+/**
+ * Amend the definition of a tracker with default properties.
+ * @param tracker 
+ * @param defaultProps 
+ * @returns 
+ */
+export function setDefaultTrackerProps(tracker: ITrackerContext, defaultProps: {[key: string]: any}): ITrackerContext {
+	return {
+		trackPageview: function (eventData?: ITrackerEventData, options?: ITrackerEventOptions): void {
+			const {props = {}, callback = undefined} = options ?? {};
+			tracker.trackPageview(eventData, { props: {...defaultProps, ...props}, callback});
+		},
+		trackEvent: function (eventName: string, options?: ITrackerEventOptions, eventData?: ITrackerEventData): void {
+			const {props = {}, callback = undefined} = options ?? {};
+			tracker.trackEvent(eventName, { props: {...defaultProps, ...props}, callback}, eventData);
+		}
+	};
+}
+
